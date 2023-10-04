0ed5d58898fdb5b9aeb7405d04d92ab723994387
vvvdanilsss <shoodanyaters@gmail.com>
2023-10-04 23:41:19 +0300

    Initial commit

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET'])
+def hello():
+    name = request.args.get('name', 'World')
+    message = request.args.get('message', 'No message')
+    return f'Hello {name}!\n{message}'
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
